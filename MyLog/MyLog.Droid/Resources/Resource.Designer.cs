186ca401d703715f852f4f658566e992e7f01e35
Sergey Puzyrny <SergeyPuzyrny@coherentsolutions.com>
2018-01-25 14:07:34 +0300

    Added logo icon

--- MyLog/MyLog.Droid/Resources/Resource.Designer.cs
+++ MyLog/MyLog.Droid/Resources/Resource.Designer.cs
@@ -87,7 +87,10 @@ namespace MyLog.Droid
 		{
 			
 			// aapt resource value: 0x7f020000
-			public const int ic_splash_loading = 2130837504;
+			public const int ic_logo = 2130837504;
+			
+			// aapt resource value: 0x7f020001
+			public const int ic_splash_loading = 2130837505;
 			
 			static Drawable()
 			{
