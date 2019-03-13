0b81db4189dc0411fa989fd7379ca6619eae69d7
Sergey Puzyrny <serg.p94@gmail.com>
2019-03-13 11:15:59 +0300

    Added ListDialog

---
+++ MyLog/MyLog.Droid/Views/Dialogs/ListDialogView.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using MvvmCross.Droid.Views.Attributes;
+using MvvmCross.Droid.Views.Fragments;
+using MyLog.Core.ViewModels.Dialogs;
+
+namespace MyLog.Droid.Views.Dialogs
+{
+    [MvxDialogFragmentPresentation]
+    public class ListDialogView : MvxDialogFragment<ListDialogViewModel>
+    {
+        public override Dialog OnCreateDialog(Bundle savedInstanceState)
+        {
+            var builder = new AlertDialog.Builder(Context);
+            builder.SetSingleChoiceItems(ViewModel.Options.Select(o => o.ToString()).ToArray(), 0, ItemSelected);
+
+            return builder.Create();
+        }
+
+        private void ItemSelected(object s, DialogClickEventArgs e)
+        {
+            Dismiss();
+            ViewModel.CloseCompletionSource.SetResult(ViewModel.Options[e.Which]);
+        }
+    }
+}
